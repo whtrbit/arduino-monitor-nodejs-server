ca39526689cd7dabfd67d4085e1a33d020caf9b3
Grzegorz Bielak (PGS Software) <gbielak@pgs-soft.com>
2018-09-17 16:51:14 +0200

    Saving data from termomenter via Google Functions

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const board = new five.Board();
 
 const led = require('./src/modules/led/led');
 const button = require('./src/modules/button/button');
+const thermometer = require('./src/modules/thermometer/thermometer');
 
 board.on('ready', () => {
     led.blink();
-
     button.addListeners();
+    thermometer.listenOnTemperatureChange();
 });
